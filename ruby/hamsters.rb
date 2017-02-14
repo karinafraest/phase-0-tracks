c45b46d9378ba253ebab21797d13cbfaa4b04aaa
Karina Franco <karinafraest@gmail.com>
2017-02-14 11:25:59 -0600

    add questions

---
+++ ruby/hamsters.rb
@@ -0,0 +1,15 @@
+puts "Please answer the following questions about your hamster."
+puts "Hamster's name: "
+name=gets.chomp
+
+puts "Volume level [how loud is your hamster?] (1-10) "
+vol=gets.chomp
+
+puts "Fur color: "
+color=gets.chomp
+
+puts "Is your hamster a good candidate for adoption (Y/N)"
+good=gets.chomp
+
+puts "Estimated age: (if you don't know, leave blank)"
+age=gets.chomp
