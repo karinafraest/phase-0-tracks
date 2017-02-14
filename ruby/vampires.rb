a5454dcace61558ec071c68fc0a80bb2b99eb38c
Karina Franco <karinafraest@gmail.com>
2017-02-14 03:08:02 -0600

    add gather data elements

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name? "
+name=gets.chomp
+
+puts "How old are you? "
+age=gets.chomp.to_i
+
+puts "What year were you born? "
+year=gets.chomp.to_i
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)"
+garlic=gets.chomp.capitalize
+
+puts "Would you like to enroll in the company's health insurance? (Y/N)"
+insurance=gets.chomp.capitalize
