ff80224f19771a4ed7709c96ea796b8a7568ddad
Karina Franco <karinafraest@gmail.com>
2017-02-20 15:15:57 -0600

    update methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,59 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+swap="iNvEsTiGaTiOn".swapcase
+#=> “InVeStIgAtIoN”
 
-# "zom".<???>
+insert="zom".insert(1,"o")
 # => “zoom”
 
-# "enhance".<???>
+enhance_center="enhance".center(16)
+#OR
+enhance_insert="enhance".insert(0,"    ") << "    "
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+stop_upcase="Stop! You’re under arrest!".upcase
 
-# "the usual".<???>
+concat="the usual"<< " suspects"
+#OR
+concat2="the usual"+" "+ "suspects"
 #=> "the usual suspects"
 
-# " suspects".<???>
+suspect_insert="suspects".insert(0,"the usual ")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+letter_chop="The case of the disappearing last letter".chop
+#OR
+letter_slice="The case of the disappearing last letter".slice(0..38)
+#=> "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+mystery_slice="The mystery of the missing first letter".slice(1..38)
+#OR
+mystery_reverse_chop_reverse="The mystery of the missing first letter".reverse.chop.reverse
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+elementary_split_join="Elementary,    my   dear        Watson!".split.join(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+z_ord="z".ord #return the integer ordinal of a one-character string
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+times_in_string="How many times does the letter 'a' appear in this string?".count("a")
 # => 4
+
+puts"swapcase #{swap}"
+puts"#{insert}"
+puts"center:#{enhance_center}"
+puts"insert:#{enhance_insert}"
+puts"upcase: #{stop_upcase}"
+puts"concatenate <<: #{concat}"
+puts"concatenate + words: #{concat2}"
+puts"insert: #{suspect_insert}"
+puts "chop: #{letter_chop}"
+puts "slice: #{letter_slice}"
+puts"slice: #{mystery_slice}"
+puts"reverse chop reverse: #{mystery_reverse_chop_reverse}"
+puts"split join: #{elementary_split_join}"
+puts"z ord: #{z_ord}"
+puts"times in string (Count): #{times_in_string}"
