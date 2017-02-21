03ce7cfea1f3f5a6bfc64267fe0a3e09461d233b
Karina Franco <karinafraest@gmail.com>
2017-02-21 13:53:54 -0600

    Added methods to iterate and filter

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,4 @@
+#----------------------------------RELEASE 1----------------------------------------
 def greetings
 	puts "Status: initializing"
 	yield("Tom")
@@ -35,3 +36,38 @@ database.each do |movie,actor|
 end
 
 p database
+#-------------------------------------RELEASE 2---------------------------------
+numbers =[5,3,9,7,10,16,3]
+
+grades={
+	exam: 90,
+	homework1: 100,
+	homework2: 90,
+	homework3: 80,
+	homework4: 95,
+	peer1: 100,
+	peer2: 85,
+	peer3: 85
+
+}
+
+p numbers 
+#1
+#numbers.delete_if{|x| x<5}
+numbers.drop_while{|x| x<5}
+#FILTER ARRAY
+new_array=numbers.take_while{|x| x<10}
+p new_array
+
+p grades
+#HASHES
+#1
+#grades.delete_if{|key, grade| grade<90}
+#2
+#grades.reject!{|key,grade| grade<90}
+#3
+#grades.keep_if{|key,grade| grade>90}
+#grades.select!{|key,grade| grade>90}
+
+p grades
+
