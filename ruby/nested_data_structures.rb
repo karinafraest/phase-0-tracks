8303adb74cf086852a1d52105c5f6f8848917ccd
Karina Franco <karinafraest@gmail.com>
2017-02-21 16:23:00 -0600

    Create data structure for shelter

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,58 @@
+shelter={
+	dogs: {
+		kennel_1:{
+		name: 'Ruffus',
+		color: 'black and white',
+		gender: 'male',
+		age: 2,
+		habilities: [
+			'Sitting',
+			'Gives paw',
+			'Play dead'],
+		months_in_shelter: 3		
+		},		
+		
+		kennel_2:{
+			name: 'Shereen',
+			color: 'brown',
+			gender: 'female',
+			age: 0,
+			habilities:[],
+			months_in_shelter: 1,
+		},
+
+		kennel_4: {},
+		kennel_5:{}
+	},
+	cats:{
+		kennel_6:{
+			name: 'Mr. Boots',
+			color: 'calico',
+			gender: 'female',
+			age: 4,
+			habilities: [
+				'Uses litter box',
+				'able to go outside'],
+			months_in_shelter: 26,
+		},
+
+		kennel_7:{
+			name: 'Cookie',
+			color: 'black and white',
+			gender: 'female',
+			age: 1,
+			habilities: [
+				'Uses litter box',
+				'use a leash'],
+			months_in_shelter: 26
+		},
+
+		kennel_8:{},
+		kennel_9:{},
+		kennel_10:{}
+	}
+}
+
+p shelter
+#shelter -- dogs and cats --- list of dogs ---list of cats, ---data of dogs --data of cats
+
