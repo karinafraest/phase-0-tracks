dbc1ccc49f1012d4e9f28a042612a6aac1fcbf99
Karina Franco <karinafraest@gmail.com>
2017-02-21 17:44:46 -0600

    Add method to create fake name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,9 +6,34 @@
 =end
 
 def swap name
-	name_array=name.split(" ").reverse!.join(" ")
-	return name_array
+	swap_name=name.split(" ").reverse!.join(" ")
+	return swap_name
 end
 
 
-p swap "Felicia Torres"
+def fake name
+	swapped_name=swap(name)
+	name_array=swapped_name.split("")
+	name_array.map! do |letter|
+		if letter.downcase=="a"||letter=="e"||letter=="i" || letter=="o"||letter=="u"
+			if letter.downcase=="a"
+				"e"
+			elsif letter=="e"
+				"i"
+			elsif letter=="i"
+				"o"
+			elsif letter=="o"
+				"u"
+			elsif letter=="u"
+				"a"
+			end
+		elsif letter==" "
+			" "
+		else
+			letter.next
+		end
+	end
+	fake_name=name_array.join("")
+	return fake_name
+end	
+p fake "Felicia Torres"
