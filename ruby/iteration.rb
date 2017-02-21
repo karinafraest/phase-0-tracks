5e128b997fd560db251a882e146bbcac236e184e
Karina Franco <karinafraest@gmail.com>
2017-02-21 12:30:46 -0600

    Add array and hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -4,4 +4,15 @@ def greetings
 	puts "Status: end"
 end
 
-greetings {|x| puts "Hello #{x}"}
+#greetings {|x| puts "Hello #{x}"}
+
+movies=["Lord of the rings", "The heat", "Kill Bill", "Pursuit of happyness", "Stomp the yard","The Internship"]
+
+database={
+	lord_of_the_rings: "Elijah Wood",
+	the_heat: "Melissa Mcarthy",
+	kill_bill: "Uma Truman",
+	pursuit_of_happyness: "Will Smith",
+	stomp_the_yard: "Meagan Good",
+	the_internship: "Vince Vaughn"
+}
