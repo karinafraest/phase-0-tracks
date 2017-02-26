4b90f7f38cf5ef8b142f3ef95f0ee2820ac80e82
Karina Franco <karinafraest@gmail.com>
2017-02-26 04:17:24 -0600

    Add happy yell method

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
 	def self.yell_angrily(words)
 		words+"!!!"+ ": ("
@@ -10,3 +11,30 @@ end
 
 p Shout.yell_angrily("This is horrible")
 p Shout.yelling_happily("Yeepy")
+=end
+
+module Shout
+	def scream(words)
+		puts "#{words.upcase}!!!!!!"
+	end
+
+	def happy_yell(words)
+		uts "#{words}!!!:D"
+	end
+end
+
+class Baby
+	include Shout
+end
+
+class Banshee
+	include Shout
+end
+
+baby=Baby.new
+baby.scream("Milk,now")
+baby.happy_yell("Mommy")
+
+banshee=Banshee.new
+banshee.scream("Death, now")
+banshee.happy_yell("I love flying")
