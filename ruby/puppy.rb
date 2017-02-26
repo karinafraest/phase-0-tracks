e9d55d2f2e7a444d446b101ed772e00af25c5a26
Karina Franco <karinafraest@gmail.com>
2017-02-26 00:16:00 -0600

    drive code for class puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+=begin
+PUPPY--------------------------------------------------------------------------
+For adoption puppies
+
+
+CHARACTERISTICS --------------------------------------------------------------
+Age: varies
+Breed: varies
+Gender: varies
+Color: varies
+Number of legs: 4
+Spayed/neutered: varies
+Up for adoption: true
+
+BEHAVIOR --------------------------------------------------------------------
+Eat
+Walk on leash
+Play dead
+Fetch
+=end
