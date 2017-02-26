7daa13985ce12bce68f5bb2372e1cf312732cff0
Karina Franco <karinafraest@gmail.com>
2017-02-26 01:33:22 -0600

    Add dev_card method for class board games

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -70,17 +70,40 @@ martin.dog_years(8)
 martin.amount_food(4,10)
 
 class Board_game
+		def initialize
+		puts "Initializing new board game instance..."
+	end
 	def roll_die (number)
+		puts "Rolling dice"
 		i=0
 		until i==number
 		die=1 + rand(6)
 		i+=1
-		puts "Die #{i+1}: #{die}"
-
+		puts "Die #{i}: #{die}"
 		end
 	end
+
+	def dev_card
+		development_cards=[]
+		14.times do
+			development_cards<<"Knight"
+		end
+		5.times do
+			development_cards<<"Victory Point"
+		end
+		2.times do
+			development_cards<<"Road Building Card"
+			development_cards<<"Monopoly Card"
+			development_cards<<"Year of Plenty"
+		end
+		card=1+rand(development_cards.length)
+		puts "You now have a #{development_cards[card]} card"
+	end
+
+
 end
 
 catan=Board_game.new
 
 catan.roll_die(2)
+catan.dev_card
