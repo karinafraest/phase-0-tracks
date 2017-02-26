b8d6495c35948a65500fd204bd4965ad6102d115
Karina Franco <karinafraest@gmail.com>
2017-02-26 01:39:06 -0600

    Add eat milk and cookies method for santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,9 +1,14 @@
 class Santa
 	def speak
-		print"Ho, ho, ho! Haaaappy holidays!"
+		puts"Ho, ho, ho! Haaaappy holidays!"
 	end #speak
+
+	def eat_milk_and_cookies(type)
+		puts "That was a good #{type}!!"
+	end #eat_milk_and_cookies
 end # class
 
 young_santa=Santa.new
 
 young_santa.speak
+young_santa.eat_milk_and_cookies("snickerdoodle")
