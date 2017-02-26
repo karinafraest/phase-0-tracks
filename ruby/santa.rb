f533ba169db079e05dc78079958cf25ccaaab93c
Karina Franco <karinafraest@gmail.com>
2017-02-26 02:12:27 -0600

    Add brag method for santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,12 @@
 class Santa
+	def initialize (gender,ethnicity)
+		puts "Initializing new instance of Santa..."
+		@gender=gender
+		@ethnicity=ethnicity
+		@reindeer_ranking=["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age=0
+	end
+
 	def speak
 		puts"Ho, ho, ho! Haaaappy holidays!"
 	end #speak
@@ -7,12 +15,40 @@ class Santa
 		puts "That was a good #{type}!!"
 	end #eat_milk_and_cookies
 
-	def initialize
-		puts "Initializing new instance of Santa..."
-	end
+	def brag
+		if !(@gender=="prefer not to say" || @gender=="N/A")
+			print "I am the best #{@gender} santa there is!"
+			if @ethnicity=="prefer not to say" || @ethnicity=="N/A"
+				puts
+			else
+				puts" Not to mention I am #{@ethnicity}!" 
+			end
+		else
+			if !(@ethnicity=="prefer not to say" || @ethnicity=="N/A")
+				puts "I am the best #{@ethnicity} santa there is!"
+			end
+		end
+	end #brag
 end # class
 
-young_santa=Santa.new
+#DRIVER CODE RELEASE 0
+#young_santa=Santa.new
 
-young_santa.speak
-young_santa.eat_milk_and_cookies("snickerdoodle")
+#young_santa.speak
+#young_santa.eat_milk_and_cookies("snickerdoodle")
+
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+santas << Santa.new("N/A", "indian")
+
+santas.each do |santa|
+	santa.speak
+	santa.brag
+	santa.eat_milk_and_cookies("chocolate chip cookie")
+end
