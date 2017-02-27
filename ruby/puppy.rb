17a1062d7e159840a843693fa2c6009c7473c5f8
Karina Franco <karinafraest@gmail.com>
2017-02-27 14:52:39 -0600

    Define class puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,33 @@
+=begin
+PUPPY--------------------------------------------------------------------------
+For adoption puppies
+
+
+CHARACTERISTICS --------------------------------------------------------------
+Age: varies
+Breed: varies
+Gender: varies
+Color: varies
+Number of legs: 4
+Spayed/neutered: varies
+Up for adoption: true
+
+BEHAVIOR --------------------------------------------------------------------
+Eat
+Walk on leash
+Play dead
+Fetch
+
+=end
+
+#define class
+class Puppy
+
+end
+
+#create instances of that class
+fido=Puppy.new
+luna=Puppy.new
+
+
+
