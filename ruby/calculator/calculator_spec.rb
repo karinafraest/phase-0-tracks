a790e33685ea6b6cceb79b5a5875d91db688fb57
Karina Franco <karinafraest@gmail.com>
2017-02-27 10:58:06 -0600

    Add test for divide that will fail

--- ruby/calculator/calculator_spec.rb
+++ ruby/calculator/calculator_spec.rb
@@ -7,7 +7,6 @@
 # 'describe' block for each group of tests  
   # 'it' block for each individual test
     # expect(<YOUR CODE>).to eq <RESULT>
-
 require_relative 'calculator'
 
 describe Calculator do
@@ -24,4 +23,9 @@ describe Calculator do
   it "multiplies two integers" do
     expect(calculator.multiply(2,3)).to eq 6
   end
+  #this will fail
+  it "divides two integers" do
+  expect(calculator.divide(10,2)).to eq 5
+  end 
+
 end
