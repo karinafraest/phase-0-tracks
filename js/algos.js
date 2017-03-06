6d37935ce33e7fb7c02412ca9161c11cabe42836
Karina Franco <karinafraest@gmail.com>
2017-03-06 10:34:22 -0600

    Pseudocode function that finds longest word or phrase

---
+++ js/algos.js
@@ -0,0 +1,7 @@
+//Input: An array with phrases
+//Steps: Asume the first phrase is the longest
+//       Compare the lenght of longest phrase with the lenght of next phrase
+//       If the length is bigger, update your longest phrase with this one
+//       Keep doing it until you are done with the string
+//Output: longest phrase
+
