2ac0d4df3b51251b994e0958494232c1d0433b9c
Karina Franco <karinafraest@gmail.com>
2017-03-06 10:41:28 -0600

    Create function longPhrase that returns the longest phrase in an array

--- js/algos.js
+++ js/algos.js
@@ -5,3 +5,20 @@
 //       Keep doing it until you are done with the string
 //Output: longest phrase
 
+function longPhrase(list){
+	var longestPhrase=list[0];
+	for(var i=1; i<list.length; i++){
+		if(list[i].length>longestPhrase.length){
+			longestPhrase=list[i];
+		}
+	}
+	return longestPhrase
+}
+
+var list=["long phrase", "even longer phrase", "the longest phrase ever", "just a phrase"];
+
+console.log(longPhrase(list))
+
+var otherList=["Ruby", "Python", "C++","JavaScript", "JQuery","HTML", "Css"];
+
+console.log(longPhrase(otherList))
