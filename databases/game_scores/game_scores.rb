46a48470149133954cfb20ae7ada9c47ef715ed0
Karina Franco <karinafraest@gmail.com>
2017-03-15 16:22:07 -0500

    Create pseudocode for a game score saver

---
+++ databases/game_scores/game_scores.rb
@@ -0,0 +1,12 @@
+#CREATE A DATABASE FOR GAMES
+#CREATE A DATABASE FOR PLAYERS
+#CREATE A DATABASE FOR SCORES
+#SELECT A GAME
+	#If the game is not in the database, add it. 
+#SELECT DATE
+	#If it is today, add today's date, if not, add date dd/mm/yyyy
+#SELECT NUMBER OF PLAYERS
+	#For each player ask name
+	#For each player ask score
+#user can STORE INFORMATION IN A DATABASE
+#user can DISPLAY INFORMATION FROM A DATABASE
