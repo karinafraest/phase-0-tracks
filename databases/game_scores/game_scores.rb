2f4be851eabc07d0b27313487e8bfe87c4a9d8fc
Karina Franco <karinafraest@gmail.com>
2017-03-15 18:27:39 -0500

    Modify add_game method to work with hash. Add view games method to view a list of all games

--- databases/game_scores/game_scores.rb
+++ databases/game_scores/game_scores.rb
@@ -1,6 +1,8 @@
 require 'sqlite3'
 #CREATE A NEW DATABASE
 db=SQLite3::Database.new("board_games.db")
+db.results_as_hash = true
+
 #CREATE A BOARD GAME TABLE
 	#ID NAME COST TIMES PLAYED
 create_table_cmd = <<-SQL
@@ -18,34 +20,43 @@ db.execute(create_table_cmd)
 #CREATE A SCORE TABLE
 	#ID, DATE, GAME NUMBER, GAME, PLAYER, SCORE
 #ADD A BOARD GAME TO THE LIST IF IT IS NOT ALREADY THERE
+
 def add_game(db,name,cost,times_played)
-	current_games=db.execute("SELECT name FROM board_games")
+	current_games=db.execute("SELECT * FROM board_games")
+	included=false
 	if current_games.length==0
+		puts "#{name} added."
 		db.execute("INSERT INTO board_games(name, cost, times_played) VALUES (?, ?,?)", [name, cost,times_played])
 	else
-		included=false
-		current_games.each do |games|
-			games.each do |game|
-				game=game.to_s
-				if games.include?(name)
-					included=true
-					puts "#{name} is already saved"
-				end
-			end
+		current_games.each do |game|
+			included=true if game["name"]==name
 		end
 		if included==false
-			puts "#{name}added."
-			db.execute("INSERT INTO board_games(name, cost, times_played) VALUES (?, ?,?)", [name, cost,times_played])	
+			puts "#{name} added."
+			db.execute("INSERT INTO board_games(name, cost, times_played) VALUES (?, ?,?)", [name, cost,times_played])
+		else
+			puts "#{name} was already saved."
 		end
 	end
 end
+
+
 #ADD A PLAYER TO THE LIST IF IT IS NOT ALREADY THERE
 #ADD A SCORE FOR A GAME
 #REVIEW BOARD GAME TABLE
+def view_games(db)
+	current_games=db.execute("SELECT * FROM board_games")
+	puts "Game".ljust(30)+"Cost".ljust(30)+"Times Played".rjust(30)
+	current_games.each do |game|
+		puts "#{game['name']}".ljust(30)+"#{game['cost']}".ljust(30)+"#{game['times_played']}" .rjust(30)
+	end
+end
+
 #REVIEW PLAYER TABLE
 #REVIEW SCORE TABLE
-
 
 add_game(db,"Settlers of Cattan", 60,20)
 add_game(db,"Splendor", 30.23,15)
 add_game(db, "Pandemic",28,6)
+
+view_games(db)
