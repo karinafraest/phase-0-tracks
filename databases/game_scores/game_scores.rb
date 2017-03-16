f9356eed343a99e9fb8266c061c1f6828823a943
Karina Franco <karinafraest@gmail.com>
2017-03-16 17:03:44 -0500

    Add scores using real names and real game names

--- databases/game_scores/game_scores.rb
+++ databases/game_scores/game_scores.rb
@@ -32,8 +32,9 @@ require 'sqlite3'
 			id INTEGER PRIMARY KEY,
 			date VARCHAR(255),
 			game_id INTEGER,
-			game_per_day INTEGER,
+			games_per_day INTEGER,
 			player_id INTEGER,
+			score INTEGER,
 			FOREIGN KEY(game_id) REFERENCES board_games(id),
 			FOREIGN KEY(player_id) REFERENCES players(id)
 			)	
@@ -42,9 +43,9 @@ require 'sqlite3'
 	db.execute(create_table_players)
 	db.execute(create_table_boardgames)
 	db.execute(create_table_scores)
-	
+
 #ADD A BOARD GAME TO THE LIST IF IT IS NOT ALREADY THERE
-def add_game(db,name,cost,times_played=0)
+def add_game(db,name,times_played=0, cost=0)
 	current_games=db.execute("SELECT * FROM board_games")
 	included=false
 	if current_games.length==0
@@ -61,6 +62,7 @@ def add_game(db,name,cost,times_played=0)
 			puts "#{name} was already saved."
 		end
 	end
+	current_games
 end
 
 
@@ -82,9 +84,41 @@ def add_player(db,first_name,last_name)
 			puts "#{first_name} #{last_name} was already saved"
 		end
 	end
+	current_names
+end
+
+def add_score(db,date,game_id,player_id,score,games_per_day)
+	db.execute("INSERT INTO scores(date,game_id,games_per_day,player_id,score) VALUES(?,?,?,?,?)",[date,game_id,games_per_day,player_id,score])
+end
+
+def find_id(db,type,name)
+	if type=="game"
+		current_games=add_game(db,name,1) #Add 1 to times played PENDING
+		this_id=db.execute("SELECT id FROM board_games WHERE 'name'=?",[name])
+	else
+		name=name.split(" ")
+		first_name=name[0]
+		last_name=name[1]
+		current_players=add_player(db,first_name,last_name)
+		this_id=db.execute("SELECT id FROM players WHERE 'first_name'=? AND 'last_name'=?",[first_name,last_name])
+	end
+	this_id
 end
 
 #ADD A SCORE FOR A GAME
+def score_game(db,date,game,num_players,games_per_day=1)
+	game_id=find_id(db,"game",game)
+	num_players.times do |player|
+		puts "Insert player's name"
+		player_name=gets.chomp
+		player_id=find_id(db,"player",player_name)
+		puts"Insert #{player_name} score for #{game}"
+		player_score=gets.chomp.to_i
+		add_score(db,date,game_id,player_id,player_score,games_per_day)
+	end
+end
+##FIND THE ID FOR NAME AND GAME
+
 #REVIEW BOARD GAME TABLE
 def view_games(db,name="all")
 	if name=="all"
@@ -110,9 +144,9 @@ end
 #REVIEW SCORE TABLE
 
 #DRIVER CODE
-add_game(db,"Settlers of Cattan", 60,20)
-add_game(db,"Splendor", 30.23,15)
-add_game(db, "Pandemic",28,6)
+add_game(db,"Settlers of Cattan",20,60)
+add_game(db,"Splendor", 15,30.25)
+add_game(db, "Pandemic",6,28)
 
 view_games(db)
 
@@ -120,3 +154,7 @@ add_player(db,"Karina","Franco")
 add_player(db,"David","Cabrera")
 
 view_players(db)
+
+#score_game(db,"3/16/2017",1,2)
+#score_game(db,"3/11/2017",2,2,2)
+score_game(db,"3/15/2017","Settlers of Cattan", 2)
