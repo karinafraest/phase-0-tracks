1d2a2dde32e2dda60d41549e9f5b210e56aabc6a
Karina Franco <karinafraest@gmail.com>
2017-03-21 18:06:21 -0500

    Add css file to create color divs

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -7,12 +7,13 @@ function randomNumber(){
 /*function that creates a line and adds it to the page*/
 function addColor(){
 	var newColor="rgb("+randomNumber()+","+randomNumber()+","+randomNumber()+")"
-	var $newColorBlock=$('<div class="change"><p>Hello</p></div>');
+	var $newColorBlock=$('<div class="change"></div>');
 	
+	$('main').append($newColorBlock);
 	$('div.change').each(function(){
 		$(this).css('background-color',newColor);
 		$(this).removeClass("change").addClass("still");
 	});
 		
-	$('main').append($newColorBlock);
+	
 }
