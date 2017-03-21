193e493069b40761ed154ca69c659158aa7a3a62
Karina Franco <karinafraest@gmail.com>
2017-03-21 17:43:19 -0500

    Add function to create a line with a random color

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,3 +1,18 @@
+/*function that creates a random color*/
+function randomNumber(){
+	var number=Math.floor((Math.random()*255)+1)
+	return String(number)
+}
+
+/*function that creates a line and adds it to the page*/
 function addColor(){
-	console.log("I work")
+	var newColor="rgb("+randomNumber()+","+randomNumber()+","+randomNumber()+")"
+	var $newColorBlock=$('<div class="change"><p>Hello</p></div>');
+	
+	$('div.change').each(function(){
+		$(this).css('background-color',newColor);
+		$(this).removeClass("change").addClass("still");
+	});
+		
+	$('main').append($newColorBlock);
 }
